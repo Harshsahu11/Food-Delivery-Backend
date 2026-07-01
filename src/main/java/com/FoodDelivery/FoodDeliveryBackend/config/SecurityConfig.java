a146fbb2cfc4e6549b5d44ce74938af1c8ffe484
Harsh Sahu <harshsahu1109@gmail.com>
2026-07-01 11:21:34 +0530

    Some Issues solved

--- src/main/java/com/FoodDelivery/FoodDeliveryBackend/config/SecurityConfig.java
+++ src/main/java/com/FoodDelivery/FoodDeliveryBackend/config/SecurityConfig.java
@@ -44,7 +44,10 @@ public class SecurityConfig {
                                          "/api/login",
                                          "/api/food/**",
                                          "/api/orders/all",
-                                         "/api/orders/status/**"
+                                         "/api/orders/status/**",
+                                         "/v3/api-docs/**",
+                                         "/swagger-ui/**",
+                                         "/swagger-ui.html"
                                  ).permitAll()
                                  .anyRequest().authenticated())
                  .sessionManagement(session ->
